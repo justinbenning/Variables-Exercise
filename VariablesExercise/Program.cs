e739be23b882aae13ffe45b11e2e3ba52e7a96dd
JustinBenning <justin.benning@gmail.com>
2024-01-11 11:50:23 -0500

    Added used bool

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -11,8 +11,10 @@
             double winningMargin = 21;
             decimal timeToFirstTD = 3.1m;
 
-            Console.WriteLine($"The {teamName} will win their first playoff game by {winningMargin}, with a score of {score49Ers} and will score their first touchdown {timeToFirstTD} seconds in to the game");
+            Console.WriteLine($"The following statement may be {ball}\nThe {teamName} will win their first playoff game by {winningMargin}, with {a} score of {score49Ers} and will score their first touchdown {timeToFirstTD} seconds in to the game");
+            
         }
+
     }
 }
 
